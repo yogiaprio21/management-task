71d09adb068aa774b366c28949e33f936731c7b9
Yogi Aprio <yogiaprio21@gmail.com>
2025-12-31 14:52:09 +0700

    Update client.ts configuration

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://management-task-ezww.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
